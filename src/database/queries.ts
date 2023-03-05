a82a521fa8057215efb5acdba8179fc8d0332cbb
Daniela Tocua <danidatope@gmail.com>
2023-03-05 10:33:31 -0500

    fixed tsconfig

---
+++ src/database/queries.ts
@@ -0,0 +1,12 @@
+import "dotenv/config";
+
+import pg from "pg";
+
+const Pool = pg.Pool;
+const pool = new Pool({
+	user: process.env.PG_USER,
+	host: process.env.PG_HOST,
+	database: process.env.PG_DB_NAME,
+	password: process.env.PG_PASSWORD,
+	port: parseInt(process.env.PG_PORT || "3000"),
+});
