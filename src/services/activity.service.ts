42430c1d64ecc1243533087f4103be6b33cc7b58
Diana Monroy <dmonroym@unal.edu.co>
2023-03-25 19:52:14 -0500

    es_plan added in findAll

--- src/services/activity.service.ts
+++ src/services/activity.service.ts
@@ -10,9 +10,9 @@ export const findAll = async (): Promise<QueryResult<any>> => {
     // Connects to the DB
     const client = await pool.connect();
     // Query all the occurrences
-    const result = client.query(`SELECT id_actividad, titulo_actividad, ubicacion, rango_precio, descripcion, restriccion_edad, medio_contacto, id_categoria FROM plan WHERE es_aprobado IS true AND es_privada IS false
+    const result = client.query(`SELECT id_actividad, titulo_actividad, ubicacion, rango_precio, descripcion, restriccion_edad, medio_contacto, id_categoria, es_plan FROM plan WHERE es_aprobado IS true AND es_privada IS false
     UNION
-    SELECT id_actividad, titulo_actividad, ubicacion, rango_precio, descripcion, restriccion_edad, medio_contacto,id_categoria FROM evento WHERE es_aprobado IS true AND es_privada IS false`);
+    SELECT id_actividad, titulo_actividad, ubicacion, rango_precio, descripcion, restriccion_edad, medio_contacto,id_categoria, es_plan FROM evento WHERE es_aprobado IS true AND es_privada IS false`);
     client.release();
     return result
 }
