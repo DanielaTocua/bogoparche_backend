dfdcd754e01e1e59e46e9d663be9ca79471236bb
Daniela Tocua <danidatope@gmail.com>
2023-04-18 10:57:49 -0500

    get all categories method key changed

--- src/services/category.service.ts
+++ src/services/category.service.ts
@@ -1,11 +1,11 @@
 import pool from "../database/pool";
 
 class categoryService {
-	async getAll(): Promise<string[]> {
+	async getAll(): Promise<{id_categoria:string,nombre_categoria:string }[]> {
 		const query = (await pool.query("SELECT * FROM categoria")).rows;
 
 		const categories = query.reduce((acc, obj) => {
-			acc[obj.id_categoria] = obj.nombre_categoria;
+			acc[obj.nombre_categoria] = obj.id_categoria ;
 			return acc;
 		}, {});
 		return categories;
