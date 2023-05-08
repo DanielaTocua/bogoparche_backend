67c655de91279d1041726715800da4768e4e4d49
Diana Monroy <dmonroym@unal.edu.co>
2023-05-08 00:47:55 -0500

    calificationAdded fixed

---
+++ src/migration/1683524660561-calificacionAdded.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CalificacionAdded1683524660561 implements MigrationInterface {
+    name = 'CalificacionAdded1683524660561'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "commentPlan" ADD "calificacion" integer NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "commentEvent" ADD "calificacion" integer NOT NULL`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "commentEvent" DROP COLUMN "calificacion"`);
+        await queryRunner.query(`ALTER TABLE "commentPlan" DROP COLUMN "calificacion"`);
+    }
+
+}
