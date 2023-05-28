bc8e42f545b799f50e63f870674cb7dd61b0a366
Daniela Tocua <danidatope@gmail.com>
2023-05-28 14:47:32 -0500

    images working on cloud

--- src/services/image.service.ts
+++ src/services/image.service.ts
@@ -1,34 +1,33 @@
 import fs from "fs";
 import path from "path";
+import {v2 as cloudinary} from "cloudinary";
+import { cloudConfig } from "../cloudConfig";
+
+
+cloudConfig;
 class ImageService {
+	
+
+	
 	async uploadImage(base64Image: string): Promise<string> {
+		
 
-		const buffer = Buffer.from(base64Image.split(",")[1], "base64");
-		const fileExtension = base64Image.split(";")[0].split("/")[1];
-        console.log(fileExtension)
-		const filename = `file_${Date.now()}.${fileExtension}`;
-		const destinationFolder = "../uploads";
-		const filePath = path.join(__dirname, destinationFolder, filename);
-		fs.writeFileSync(filePath, buffer);
 
+		const filename = "file_"+Date.now() as string ;
+		await cloudinary.uploader.upload(base64Image,{public_id: filename, folder: "uploads"});	
+		
+		
 		return filename;
 	}
 
 	async getBase64Image(filename: string): Promise<string> {
-		const filePath = path.join(__dirname, "../uploads", filename);
-		console.log(filePath);
-		const buffer = fs.readFileSync(filePath);
-		const base64Image = buffer.toString("base64");
-		return `data:image/${path
-			.extname(filename)
-			.slice(1)};base64,${base64Image}`;
+
+		return cloudinary.url("uploads/"+filename)
 	}
 
-	deleteImage(filename: string) {
-		const filePath = path.join(__dirname, "../uploads", filename);
-        console.log(filePath)
-		fs.unlinkSync(filePath);
-        console.log("jay")
+	async deleteImage(filename: string) {
+		cloudinary.uploader.destroy("uploads/" + filename, function(error,result) {
+			console.log(result, error) });		
 	}
 }
 export default new ImageService();
