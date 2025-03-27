c18f7bafaf0b2c2cc87dc423908cf93a3ceb414a
Daniela Tocua <danidatope@gmail.com>
2025-03-27 10:38:39 -0500

    fix: remove captcha validation

--- src/routes/event.routes.ts
+++ src/routes/event.routes.ts
@@ -5,7 +5,6 @@ import { NewEventEntryDTO } from "../dtos/activity.dto";
 import asyncErrorMiddleware from "../middlewares/asyncError.middleware";
 import authMiddleware from "../middlewares/auth.middleware";
 import checkAccessMiddleware from "../middlewares/checkAccess.middleware";
-import checkCaptchaMiddleware from "../middlewares/checkCaptcha.middleware";
 import dtoValidationMiddleware from "../middlewares/dtoValidation.middleware";
 import idNumberValidationMiddleware from "../middlewares/idNumberValidation.middleware";
 // import toNewActivityEntry from '../utils/utils_activity'
@@ -27,7 +26,6 @@ router
 	.put(
 		[
 			authMiddleware,
-			checkCaptchaMiddleware,
 			idNumberValidationMiddleware,
 			checkAccessMiddleware,
 		],
--- src/routes/plan.routes.ts
+++ src/routes/plan.routes.ts
@@ -5,7 +5,6 @@ import { NewPlanEntryDTO } from "../dtos/activity.dto";
 import asyncErrorMiddleware from "../middlewares/asyncError.middleware";
 import authMiddleware from "../middlewares/auth.middleware";
 import checkAccessMiddleware from "../middlewares/checkAccess.middleware";
-import checkCaptchaMiddleware from "../middlewares/checkCaptcha.middleware";
 import dtoValidationMiddleware from "../middlewares/dtoValidation.middleware";
 import idNumberValidationMiddleware from "../middlewares/idNumberValidation.middleware";
 // import toNewActivityEntry from '../utils/utils_activity'
@@ -19,7 +18,6 @@ router
 	.post(
 		[
 			authMiddleware,
-			checkCaptchaMiddleware,
 			dtoValidationMiddleware(NewPlanEntryDTO),
 		],
 		asyncErrorMiddleware(planController.addPlan),
